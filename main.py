6bd186da45f96efc2444d5243cf6ec9f07f68eb7
joshgallantt <77059265+joshgallantt@users.noreply.github.com>
2021-06-19 22:57:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 import bs4
-from os import system, name
 import csv
 
+#create a book object with data we want to scrape
 class Book:
     def __init__(self, page_url, title = None, description = None, category = None, rating = None, upc = None, price = None, stock = None):
         self.page_url = page_url
@@ -23,6 +23,10 @@ class Book:
     def __repr__(self):
         return repr(self.title)
 
+
+list_of_books_on_current_page = []
+final_list_of_books = []
+current_page = 49
 ratings = {
   "One": 1,
   "Two": 2,
@@ -31,27 +35,23 @@ ratings = {
   "Five": 5
 }
 
-page_of_books = []
-list_of_books = []
 url = "https://books.toscrape.com/catalogue/"
-current_page = 49
 
 while True:
-    #make the soup equal to the page we are on
+    #make the soup equal to the page we are on (.content is important here for symbol decoding), set the next page text
     soup = bs4.BeautifulSoup(requests.get(url + f"page-{current_page}.html").content,"lxml")
     html_books = soup.select(".product_pod")
     next_page_text = soup.find_all("a")[-1].text
 
     print(f"\n Currently processing page {current_page}:\n")
     
-    # for each book on this page, get the link to each book
+    # for each book on this page, create a book object with a link to each book, and create a list of these book objects
     for html_book in html_books:
         bookpage = html_book.select("a")[0]['href']
-        page_of_books.append(Book(url + bookpage))
+        list_of_books_on_current_page.append(Book(url + bookpage))
     
-    # go through each book on the page and add atributes to each book object
-
-    for i, book in enumerate(page_of_books):
+    # for each book on the current page, go to their individual page and scrape the data
+    for i, book in enumerate(list_of_books_on_current_page):
         soup = bs4.BeautifulSoup(requests.get(book.page_url).content,"lxml")
         book.title = soup.select(".active")[0].text
         book.description = soup.findAll("p")[3].text
@@ -63,18 +63,19 @@ while True:
 
     # after finishing the page, move the books to the final list
         print(f"Data gathered for {book.title}")
-        list_of_books.append(page_of_books.pop(i))
+        final_list_of_books.append(list_of_books_on_current_page.pop(i))
 
+    # check if the page has a next button, if it does select that page and restart the loop
     if next_page_text == 'next':
         current_page +=1
     else:
         break
 
-# write to file
-with open('bookstoscrape.csv', 'w', encoding='UTF8', newline='') as f:
+# write to file, utf-8-sig for excel
+with open('bookstoscrape.csv', 'w', encoding='utf-8-sig', newline='') as f:
     writer = csv.writer(f)
     writer.writerow(['Category', 'Title', 'Price', 'Rating','Stock','UPC', 'Description'])
 
-    for book in list_of_books:
+    for book in final_list_of_books:
         data = [book.category, book.title, book.price, book.rating, book.stock, book.upc, book.description]
         writer.writerow(data)
